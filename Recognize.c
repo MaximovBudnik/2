f6af08ae2a9ed35008b6862f0e43dfc20c37f3e7
MaximovBudnik <maximovlive@gmail.com>
2014-12-24 23:42:02 +0300

    Update Recognize.c

--- Recognize.c
+++ Recognize.c
@@ -5,26 +5,35 @@ MAI
 #include <string.h>
 
 FILE *file;
-int tb[1000][1000];
+int mat[1000][1000];
 int otvet[100];
-int x,y,h,g,kvp,itv,i,j;
+int x,y,h,g,k,itv,i,j;
 
-void tbbuild();
+void form();
 void recognize();
-void chekmark();
-void otvprint();
-void tbshow();
+void verscore();
+void otvout();
+void present();
 
 int main(int argc, char *argv[]){
 	file = fopen(argv[1], "r");
-	tbbuild();
+	form();
 	recognize();
-	chekmark();
-	otvprint();
+	verscore();
+	otvout();
 
 }
 
-void tbbuild(){
+void verscore(){
+	int vpr,otv;
+	for (vpr=1;vpr<k+1;vpr++){
+		for (otv=1;otv<itv+1;otv++){
+			if ((mat[((1+g)/2+1)+(1+g)*(vpr-1)][((1+h)/2+1)+(1+h)*(otv-1)]) == 1) otvet[vpr]=otv;
+		}
+	}
+}
+
+void form(){
 	char ch[2];
 	i = 1;
 	y = 1;
@@ -40,13 +49,13 @@ void tbbuild(){
 		}
 	if (ch[0] == '1' && y>=3)
 		{
-			tb[y-2][i] = 1;
+			mat[y-2][i] = 1;
 			i++;
 		}
 	
 	if (ch[0] == '0' && y>=3)
 		{
-		tb[y-2][i] = 0;
+		mat[y-2][i] = 0;
 		i++;
 		}
 	ch[0] = 0;
@@ -55,40 +64,34 @@ void tbbuild(){
 	x=x-1;
      }
 
-void recognize(){
-	for (i=1,j=0;j<2;i++){
-		if (tb[2][i] == 1) j++;
-		if (tb[2][i] == 0) h++;
-	}
-	for (i=1,j=0;j<2;i++){
-		if (tb[i][2] == 1) j++;
-		if (tb[i][2] == 0) g++;
-	}
-	itv=(x-1)/(h+1);
-	kvp=(y-1)/(g+1);
-}
-
-void chekmark(){
-	int vpr,otv;
-	for (vpr=1;vpr<kvp+1;vpr++){
-		for (otv=1;otv<itv+1;otv++){
-			if ((tb[((1+g)/2+1)+(1+g)*(vpr-1)][((1+h)/2+1)+(1+h)*(otv-1)]) == 1) otvet[vpr]=otv;
-		}
-	}
-}
-
-void otvprint(){
-	for (i=1;i<kvp+1;i++)
-		printf("%d\n",otvet[i]);
-}
-
-void tbshow(int y, int x){
+void present(int y, int x){
 	for (i=1;i<y+1;i++){
 		for (j=1;j<x+1;j++){
-			printf("%d",tb[i][j]);
+			printf("%d",mat[i][j]);
 		}
 		printf("\n");
 	}
 }
+
+void recognize(){
+	for (i=1,j=0;j<2;i++){
+		if (mat[2][i] == 1) j++;
+		if (mat[2][i] == 0) h++;
+	}
+	for (i=1,j=0;j<2;i++){
+		if (mat[i][2] == 1) j++;
+		if (mat[i][2] == 0) g++;
+	}
+	itv=(x-1)/(h+1);
+	k=(y-1)/(g+1);
+}
+
+
+
+void otvout(){
+	for (i=1;i<k+1;i++)
+		printf("%d\n",otvet[i]);
+}
+
 
 Budnik A.N.
