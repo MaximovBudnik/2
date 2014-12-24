dd301e1aae2fa6339d39e3e6bec1f4b12dbdcbd2
MaximovBudnik <maximovlive@gmail.com>
2014-12-24 23:41:21 +0300

    Update Capture.c

--- Capture.c
+++ Capture.c
@@ -4,15 +4,15 @@ MAI
 #include <stdlib.h>
 #include <string.h>
 
-int tbl[1000][1000];
-int a,b,c,d,i,f;
+int form[1000][1000];
+int a,b,c,d,i,j;
 char *args[100];
 
-void exeptions(int argc);
-void prisv();
-void tblconstruct();
-void setmrk(int vpr, int otv);
-void tblshow();
+void exclusion(int argc);
+void assig();
+void desig();
+void settick(int vpr, int otv);
+void present();
 
 int main(int argc, char *argv[]){
 	int k;
@@ -20,15 +20,15 @@ int main(int argc, char *argv[]){
 	for (k=1;k<argc-1;k++)
 		args[k]=argv[k];
 		
-	exeptions(argc);
-	prisv();
-	tblconstruct();
+	exclusion(argc);
+	assig();
+	desig();
 	for (k=5;k<argc;k++)
-		setmrk(k-4,atoi(argv[k]));
-	tblshow();
+		settick(k-4,atoi(argv[k]));
+	present();
 }
 
-void exeptions(int argc){
+void exclusion(int argc){
 	if (argc == 1) {
 		printf("\nПравило заполнения аргументов: (ширина ячейки) (высота ячейки) (количество столбцов) (количество строк) (ответы)\n\n");
 		exit(1);
@@ -37,7 +37,7 @@ void exeptions(int argc){
 		printf("\nВсего вопросов %d, а вы ввели %d ответов\n\n",atoi(args[4]),argc-5);
 		exit(1);
 	}
-	for (i=5;i<argc-1;i++)
+	for (i=5;i<argc-1;i++) 
 		if (atoi(args[3]) < atoi(args[i]) || atoi(args[i]) < 1){
 			printf("\nВсего вариантов ответа %d, а вы ввели ответ %d\n\n",atoi(args[3]),atoi(args[i]));
 			exit(1);
@@ -48,41 +48,42 @@ void exeptions(int argc){
 	}
 }
 
-void prisv(){
+void assig(){
 	c=atoi(args[1]);
 	d=atoi(args[2]);
 	a=(atoi(args[1])+1)*atoi(args[3])+1;
 	b=(atoi(args[2])+1)*atoi(args[4])+1;
 }
 
-void tblconstruct(){
+void desig(){
 	for (i=1;i<b+1;i=i+d+1)
-		for (f=1;f<a+1;f++)
-			tbl[i][j]=1;
+		for (j=1;j<a+1;j++)
+			form[i][j]=1;
 			
-	for (f=1;f<a+1;f=f+c+1)
+	for (j=1;j<a+1;j=j+c+1)
 		for (i=1;i<b+1;i++)
-			tbl[i][f]=1;
+			form[i][j]=1;
 }
 
-void setmrk(int vpr, int otv){
+void settick(int vpr, int otv){
 	int m,n;
 	m=3+((c+1)*(otv-1));
 	n=3+((d+1)*(vpr-1));
 	for (i=0;i<d-2;i++)
-		for (f=0;f<c-2;f++)
-			tbl[n+i][m+f]=1;
+		for (j=0;j<c-2;j++)
+			form[n+i][m+j]=1;
 }
 
-void tblshow(){
+void present(){
 	printf("P1\n");
 	printf("%d %d\n",a,b);
 	for (i=1;i<b+1;i++){
-		for (f=1;f<a+1;f++){
-			printf("%d",tbl[i][f]);
+		for (j=1;j<a+1;j++){
+			printf("%d",form[i][j]);
 		}
 		printf("\n");
 	}
 }
+
 
 Maksimov A.N.
